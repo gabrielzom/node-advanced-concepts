83afcd2c2e450b03f00acffb88e1f05acbe2e9d6
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-10 09:44:41 -0300

    section 1 - video 8

---
+++ loop.js
@@ -0,0 +1,15 @@
+// node myFile.js
+
+myFile.runContents();
+
+function shouldContinue() {
+
+}
+
+// Entire body executes in one 'tick'
+while(shouldContinue()) {
+  
+}
+
+
+// exit back to terminal
