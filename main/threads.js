6c89400149e2793ddc4f6e0f9c8184ff1088a521
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-11 10:00:39 -0300

    multithreading testing

--- main/threads.js
+++ main/threads.js
@@ -2,6 +2,25 @@ const crypto = require('crypto')
 
 const start = Date.now()
 
+
+// Testing with 4 threads of libuv.
+
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-  console.log('1:', Date.now - start)
+  console.log('1:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('2:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('3:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('4:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('5:', Date.now() - start)
 })
