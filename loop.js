6705ff79a7004aeb1c6d8f2e66be6e06ac242d0e
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-11 08:33:26 -0300

    logical test in loop

--- loop.js
+++ loop.js
@@ -1,14 +1,33 @@
 // node myFile.js
 
-myFile.runContents();
+const pendingTimers = [1, 2, 3, 4]
+const pendingOSTasks = [1, 2, 3, 4, 5, 6, 7]
+const pendingOperations = [1, 2]
+
+//myFile.runContents();
 
 function shouldContinue() {
 
+  if (pendingOSTasks.length > 0) {
+    pendingOSTasks.shift()
+
+  } else if (pendingTimers.length > 0) {
+    pendingTimers.shift()
+
+  } else if (pendingOperations.length > 0) {
+    pendingOperations.shift()
+  }
+
+  return (
+    pendingTimers.length +
+    pendingOSTasks.length +
+    pendingOperations.length
+  )
 }
 
 // Entire body executes in one 'tick'
 while(shouldContinue()) {
-  
+  console.log('Executing... ', shouldContinue())
 }
 
 
