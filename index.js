b49919072cdcc3013fab96a9263ecc7b2ceb8260
Gabriel S Nascimento <gabriel.nascimento@skyone.solutions>
2022-05-12 00:13:32 -0300

    using web-worker

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-process.env.UV_THREADPOOL_SIZE = 1
-const cluster = require('cluster')
+const express = require('express')
+const app = express();
+const port = 8000 || process.env.PORTS
+const crypto = require('crypto')
+var token = 'Bearer '
 
-// Is the file being executed in mater mode ?
-if (cluster.isMaster) {
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-  cluster.fork()
-
-} else {
-  const express = require('express')
-  const app = express();
-  const port = 8000 || process.env.PORTS
-  const crypto = require('crypto')
-  var token = 'Bearer '
-  
-  app.get('/', (request, response) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      response.send('Hi there !')
-    })
+app.get('/', (request, response) => {
+  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    response.send('Hi there !')
   })
+})
 
+app.get('/fast', (request, response) => {
+  const { data } = (crypto.pbkdf2Sync('Sk!0n3.S0lut1on5', (Math.random()).toString(), 1000, 32, 'sha256')).toJSON()
+  data.forEach((item) => { token += item })
 
-  app.get('/fast', (request, response) => {
-    const { data } = (crypto.pbkdf2Sync('Sk!0n3.S0lut1on5', (Math.random()).toString(), 1000, 32, 'sha256')).toJSON()
-    data.forEach((item) => { token += item })
-  
-    response.setHeader('Authorization', token)
-    response.status(200).send('This was fast !')
-  })
-  
-  
-  
-  app.listen(port, () => {
-    console.log(`Server listen in http://localhost:${port}`)
-  })
-}
+  response.setHeader('Authorization', token)
+  response.status(200).send('This was fast !')
+})
 
-
+app.listen(port, () => {
+  console.log(`Server listen in http://localhost:${port}`)
+})
